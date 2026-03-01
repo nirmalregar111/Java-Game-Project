338f5187000e6ab8b757954d9f5262f4822f420c
NirmalDz <nirmalregar111@gmail.com>
2026-03-01 22:18:08 +0530

    Implemented Encapsulation and improved game logic

--- Player.java
+++ Player.java
@@ -1,14 +1,35 @@
 public class Player {
 
-    String name;
-    int health;
-    boolean hasKey;
-
-    // Constructor
+    private String name;
+    private int health;
+    private boolean hasKey;
+    
+    //constructor
     public Player(String name) {
         this.name = name;
         this.health = 100;
         this.hasKey = false;
+    }
+
+    // Health reduce method
+    public void reduceHealth(int amount) {
+    health -= amount;
+    if (health < 0) {
+        health = 0;
+    }
+}
+    // Key set method
+    public void setKey(boolean value) {
+        hasKey = value;
+    }
+
+    // Getter methods
+    public boolean hasKey() {
+        return hasKey;
+    }
+
+    public int getHealth() {
+        return health;
     }
 
     public void showStatus() {
--- Room.java
+++ Room.java
@@ -6,7 +6,7 @@ public class Room {
 
         Scanner sc = new Scanner(System.in);
 
-        while (!player.hasKey && player.health > 0) {
+        while (!player.hasKey() && player.getHealth() > 0) {
 
             System.out.println("\nYou are inside a dark room...");
             System.out.println("1. Open the box");
@@ -14,20 +14,19 @@ public class Room {
             System.out.println("3. Leave the room");
 
             System.out.print("Choose option: ");
-
             int choice = Integer.parseInt(sc.nextLine());
 
             if (choice == 1) {
                 System.out.println("🎉 You found the hidden key!");
-                player.hasKey = true;
+                player.setKey(true);
             }
             else if (choice == 2) {
                 System.out.println("❌ Nothing here! You lost 20 health.");
-                player.health -= 20;
+                player.reduceHealth(20);
             }
             else if (choice == 3) {
                 System.out.println("❌ You hit the wall! Lost 10 health.");
-                player.health -= 10;
+                player.reduceHealth(10);
             }
             else {
                 System.out.println("⚠ Invalid choice! Try again.");
@@ -36,7 +35,7 @@ public class Room {
             player.showStatus();
         }
 
-        if (player.hasKey) {
+        if (player.hasKey()) {
             System.out.println("\n🏆 YOU WIN THE GAME!");
         } else {
             System.out.println("\n💀 GAME OVER!");
